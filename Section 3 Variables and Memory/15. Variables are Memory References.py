06cd2dbf02fed9e57d895f4b934825bcb9c73c2d
Grey <sergejyurskyj@yahoo.com>
2019-08-21 23:06:04 -0700

    Commit 15. Variables and Memory

---
+++ Section 3 Variables and Memory/15. Variables are Memory References.py
@@ -0,0 +1,22 @@
+print('#' * 52 + '  We can find the memory address that a variable references, by using the id() function.')
+print('#' * 52 + '  The id() function returns the memory address of its argument as a base-10 integer.')
+print('#' * 52 + '  We can use the function hex() to convert the base-10 number to base-16.')
+
+my_var = 10
+
+print('my_var = {0}'.format(my_var))
+print('memory address of my_var (decimal): {0}'.format(id(my_var)))
+print('memory address of my_var (hex): {0}'.format(hex(id(my_var))))
+
+
+greeting = 'Hello'
+print('greeting = {0}'.format(greeting))
+print('memory address of my_var (decimal): {0}'.format(id(greeting)))
+print('memory address of my_var (hex): {0}'.format(hex(id(greeting))))
+
+print('#' * 52 + '  Note how the memory address of my_var is different from that of greeting.')
+print('#' * 52 + '  Strictly speaking, my_var is not "equal" to 10. ')
+print('#' * 52 + '  Instead my_var is a reference to an (integer) object '
+                 '  (containing the value 10) located at the memory address id(my_var)')
+print('#' * 52 + '  Similarly for the variable greeting.')
+
